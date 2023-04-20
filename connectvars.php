141685f704b60f5d24de7599678a8c1d9839ff69
Amos Johnson <63177847+AmosJAMcLovin@users.noreply.github.com>
2023-04-20 15:12:23 -0400

    Add files via upload

---
+++ connectvars.php
@@ -0,0 +1,7 @@
+<?php
+// Define the database connection constants here!
+define('DB_HOST', 'localhost');
+define('DB_USER', '{username}');
+define('DB_PASSWORD', '{XXXXXXXXXXXXXXXXXXX}');
+define('DB_NAME', '{username}db');
+?>
